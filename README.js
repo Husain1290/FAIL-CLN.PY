60e277d123d8c2f7384dffc87331b532efedefd5
Husain1290 <148206866+Husain1290@users.noreply.github.com>
2024-09-10 10:59:09 +0600

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,14 @@
+/*যোগ, বিয়োগ, গুণ, ভাগ */
+
+  let: a = 7;
+  let: b = 5;
+  let: "a + b";
+  let: c = "6"+2;
+
+      console.log (" a =", a,"! b =", b );
+
+      console.log ("a + b + c =", a + b + c);
+
+      console.log ("a - b + c =", a - b + c);
+
+      console.log (" c =", c );
--- README.md
+++
@@ -1,1 +0,0 @@
-![admin pik](https://github.com/user-attachments/assets/da627285-4057-4d19-8290-777fe5dddc39)
